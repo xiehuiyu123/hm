9569d64714670dd768e0ba678340a46c602ed75b
xie <1210459691@qq.com>
2020-04-05 19:10:01 +0800

    第一天

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="login">
-    <div class="left"></div>
+    <div class="left">
+      <div class="title">
+        <img src="@/assets/images/11.png" alt />
+        <span class="title_name1">黑马面面</span>
+        <span class="title_cen">|</span>
+        <span class="title_name2">用户登录</span>
+      </div>
+    </div>
     <div class="right">
       <img src="@/assets/images/login_banner_ele.png" alt />
     </div>
@@ -9,11 +16,25 @@
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      }
+    };
+  }
 };
 </script>
 
-<style lang="less">
+<style lang="less" >
 .login {
   height: 100%;
   display: flex;
@@ -28,6 +49,25 @@ export default {
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
+    .title {
+      padding: 47px 41px 86px 43px;
+      .title_name1 {
+        margin: 0 8px;
+        font-size: 24px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+      }
+      .title_cen {
+        font-size: 24px;
+        color: #ccc;
+      }
+      .title_name2 {
+        margin: 0 8px;
+        font-size: 22px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+      }
+    }
   }
 }
 </style>
