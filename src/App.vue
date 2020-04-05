0dd02e1ad9b7a4dbd67e74b71a476fb752027660
xie <1210459691@qq.com>
2020-04-05 10:23:55 +0800

    项目起步，删除默认代码

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<template></template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-@import url(~@/assets/home.css);
 </style>
