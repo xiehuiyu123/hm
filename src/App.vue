53820e3cc622354e60ae05afa7b61b0190d88b38
xie <1210459691@qq.com>
2020-04-05 10:59:58 +0800

    创建初始化文件夹

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<template></template>
+<template>
+  <div>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
+  </div>
+</template>
 
 <script>
 export default {};
