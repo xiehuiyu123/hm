0dab10028b83d02455279e3865dfc83466427806
xie <1210459691@qq.com>
2020-04-06 16:19:45 +0800

    删除多于

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
-    <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
-    <el-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></el-progress>
-    <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//elm 下载导入使用
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
-
 Vue.config.productionTip = false
 
 new Vue({
